881ef58272cba402106c5e21ffe7da4e16809983
Abdulsamad sadiq <otaikisadiq2804@gmail.com>
2024-02-16 02:02:40 +0100

    Update OtaikiToken.sol

--- contracts/OtaikiToken.sol
+++ contracts/OtaikiToken.sol
@@ -2,9 +2,18 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract OtaikiToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("OtaikiToken", "OTAIKI") {
+contract OtaikiToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("OtaikiToken", "OTAIKI") Ownable(msg.sender){
         _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
     }
 }
